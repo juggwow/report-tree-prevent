641e2d371ffb1ca3a9be574772c6d24e946b4434
patna <p.wiriyar@gmail.com>
2023-11-11 22:59:09 +0700

    chang title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>ผบร.กบษ.ต.3</title>
+      <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/e/e3/LOGO_PEA.jpg" />
       <body>
         <Main />
         <NextScript />
