ab12f303f3dd51a624c9a8e9836b32592c47de3b
patna <p.wiriyar@gmail.com>
2023-11-19 11:07:54 +0700

    auto login line

--- pages/linelogin.tsx
+++ pages/linelogin.tsx
@@ -1,22 +1,9 @@
-import { getCsrfToken, signIn } from "next-auth/react"
+import { signIn } from "next-auth/react"
 import { useEffect } from "react"
 
-// export async function getServerSideProps(context: any){
-//     const csrfToken = await getCsrfToken(context.res)
-//     return {
-//         props: {
-//             csrfToken: csrfToken
-//         }
-//     }
-// }
-
 export default function LineLogin(){
-
-    
-
     useEffect(()=>{
         signIn("line")
-        
         return
     },[])
     return (
