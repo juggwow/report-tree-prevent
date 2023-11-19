697238033ff41dfebcd6733fa3e5c544344c8758
patna <p.wiriyar@gmail.com>
2023-11-19 11:10:20 +0700

    auto login line and redirect

--- pages/linelogin.tsx
+++ pages/linelogin.tsx
@@ -1,5 +1,20 @@
-import { signIn } from "next-auth/react"
+import { getSession, signIn } from "next-auth/react"
 import { useEffect } from "react"
+
+export async function getServerSideProps(context: any) {
+    const session = await getSession(context);
+  
+    if (session) {
+      return {
+        redirect: {
+          destination: "/",
+        },
+      };
+    }
+    return {
+      props: {},
+    };
+  }
 
 export default function LineLogin(){
     useEffect(()=>{
