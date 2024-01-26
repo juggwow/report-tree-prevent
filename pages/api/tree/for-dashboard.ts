66222e9ddfae15b4a6b6c0530e74d4fd31083559
patna <p.wiriyar@gmail.com>
2024-01-26 16:36:35 +0700

    add get data DB

---
+++ pages/api/tree/for-dashboard.ts
@@ -0,0 +1,34 @@
+import clientPromise from "@/lib/mongodb";
+import { ObjectId, WithId } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
+import authOptions from "../auth/authoption";
+import { getServerSession } from "next-auth";
+import { AdminChangePlanTree } from "@/types/report-tree";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<any>,
+  ) {
+
+    try{
+        switch(req.method){
+            case "GET":{
+                const mongoClient = await clientPromise;
+                const data = await mongoClient.db("tree").collection("forDashBoard").find().toArray();
+        
+                res.send(data)
+                res.end()
+                return
+            }
+            default:{
+                res.status(404).end()
+                return
+            }
+        }
+    }catch(e){
+        res.status(500).end()
+        return
+    }
+
+  }
