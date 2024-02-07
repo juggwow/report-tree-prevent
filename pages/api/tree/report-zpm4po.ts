609b5a926324d9ec58b6b66d1009ff10e26d6de6
patna <p.wiriyar@gmail.com>
2024-02-07 21:22:03 +0700

    fix to use updateMany report tree

--- pages/api/tree/report-zpm4po.ts
+++ pages/api/tree/report-zpm4po.ts
@@ -8,6 +8,7 @@ import authOptions from "../auth/authoption";
 import clientPromise from "@/lib/mongodb";
 import { treeData } from "@/types/report-tree";
 import { ObjectId } from "mongodb";
+import formatDate from "@/lib/format-date";
 
 type Data = {
   massege: string;
@@ -31,35 +32,103 @@ export default async function handler(
       const planTreeCollection = mongoClient.db("tree").collection("plan");
 
       const data: treeData[] = JSON.parse(req.body);
+      console.log(data)
 
-      for(const val of data){
-        const doc = await planTreeCollection.findOne(
-          { _id: new ObjectId(val.id as string) },
-          { projection: { businessName: 1 } },
-        );
-        if (!(doc && doc.businessName == session.pea?.karnfaifa)) {
-          await mongoClient.close();
-          res.status(404).end();
-          return;
+      let editIds:ObjectId[]=[]
+      let reportIds:ObjectId[]=[]
+      let date=formatDate(new Date())
+      let order:string[]=[]
+      data.forEach((val)=>{
+        val.reportDate?reportIds.push(new ObjectId(val.id as string)):editIds.push(new ObjectId(val.id as string))
+        order.push(val.zpm4Po as string)
         }
-        const resultUpdate = await planTreeCollection.updateOne(
-          {
-            _id: new ObjectId(val.id as string),
-          },
-          {
-            $set: {
-              editReport: val.editDate,
-              reportDate: val.reportDate,
-              zpm4Po: val.zpm4Po,
-            },
-          },
-        );
-        if (!resultUpdate.acknowledged) {
-          await mongoClient.close();
-          res.status(404).end();
-          return;
+      )
+
+      order = order.filter((val,i,arr)=>{
+        return arr.indexOf(val) === i
+      })
+      if(order.length !=1){
+        res.status(405).end()
+        return
+      }
+
+      const reportfilter = {
+        _id : {$in : reportIds},
+        businessName: session.pea.karnfaifa
+      }
+      const reportDocs = await planTreeCollection.find(reportfilter).toArray()
+      if(reportDocs.length != reportIds.length){
+        await mongoClient.close()
+        res.status(404).end()
+        return
+      }
+
+      const editFilter = {
+        _id : {$in : editIds},
+        businessName: session.pea.karnfaifa
+      }
+      const editDocs = await planTreeCollection.find(editFilter).toArray()
+      if(editDocs.length != editIds.length){
+        await mongoClient.close()
+        res.status(404).end()
+        return
+      }
+
+      const reportUpdate = {
+        $set: {
+          reportDate: date,
+          editReport: date,
+          zpm4Po: order[0]
         }
       }
+      const resReport = await planTreeCollection.updateMany(reportfilter,reportUpdate)
+      if(!resReport.acknowledged){
+        await mongoClient.close()
+        res.status(500).end()
+        return
+      }
+
+      const editUpdate = {
+        $set : {
+          editReport: date,
+          zpm4Po: order[0]
+        }
+      }
+      const resEdit = await planTreeCollection.updateMany(editFilter, editUpdate)
+      if(!resEdit.acknowledged){
+        await mongoClient.close()
+        res.status(500).end()
+        return
+      }
+
+      // for(const val of data){
+      //   const doc = await planTreeCollection.findOne(
+      //     { _id: new ObjectId(val.id as string) },
+      //     { projection: { businessName: 1 } },
+      //   );
+      //   if (!(doc && doc.businessName == session.pea?.karnfaifa)) {
+      //     await mongoClient.close();
+      //     res.status(404).end();
+      //     return;
+      //   }
+      //   const resultUpdate = await planTreeCollection.updateOne(
+      //     {
+      //       _id: new ObjectId(val.id as string),
+      //     },
+      //     {
+      //       $set: {
+      //         editReport: val.editDate,
+      //         reportDate: val.reportDate,
+      //         zpm4Po: val.zpm4Po,
+      //       },
+      //     },
+      //   );
+      //   if (!resultUpdate.acknowledged) {
+      //     await mongoClient.close();
+      //     res.status(404).end();
+      //     return;
+      //   }
+      // }
       
       await mongoClient.close();
       res.status(200).send({ massege: `รายงาน ZPM4/PO สำเร็จ` });
